0f0aa15671853fdd4080470c185422a49eaf19ec
Tcanh12 <canhnvthe186121@fpt.edu.vn>
2026-08-15 23:03:57 +0700

    remove doc to clean code

--- docs/database/scripts/patches/2026-08-01_make_phone_nullable.sql
+++
@@ -1,7 +0,0 @@
--- Make phone numbers optional for visit requests
-ALTER TABLE visit_requests
-    MODIFY COLUMN registrant_phone VARCHAR(50) NULL,
-    MODIFY COLUMN contact_person_phone VARCHAR(50) NULL;
-
-ALTER TABLE visit_instance_form_details
-    MODIFY COLUMN operational_contact_phone VARCHAR(50) NULL;
